239dfdfcecdc971bfbf4183addb408c863753d9f
KiyonoOsawa <kiyo.sntr.x6@icloud.com>
2024-08-18 19:22:38 +0900

    add:オリジナル追加！

---
+++ ruby_basic_1_original.rb
@@ -0,0 +1,12 @@
+# ここにコードを書こう
+
+has_weekend_practice = true
+weekly_practice_hours = 8.5
+num_members = 20
+
+if has_weekend_practice = true && weekly_practice_hours >= 6.0 && 
+    num_members <= 30 && num_members >= 15
+    puts "理想の部活動です！"
+else
+    puts "理想とは少し違うようです"
+end
