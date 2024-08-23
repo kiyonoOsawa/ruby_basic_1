1541dd3a8ccf3e19e4561d506d1e39680899e234
KiyonoOsawa <kiyo.sntr.x6@icloud.com>
2024-08-24 02:44:40 +0900

    add:追加

--- ruby_basic_1.rb
+++ ruby_basic_1.rb
@@ -1,1 +1,12 @@
 # ここにコードを書こう
+
+has_hotel_onsen = true
+sunday_of_trips = 1
+num_members = 2
+
+if has_hotel_onsen = true && sunday_of_trips >= 0.8 && 
+    num_members <= 2
+    puts "理想の旅行です！"
+else
+    puts "理想とは少し違うようです。。。。"
+end
